a8dc7340242df5013453b2f5d39b21fe10727ae7
Ernestas Urbonas <ernesrourbonas@gmail.com>
2024-09-24 13:29:50 +0200

    Started Project

---
+++ tiendaJuegos/src/test/java/com/tienda/tiendaJuegos/MiTiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.tiendaJuegos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiTiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
